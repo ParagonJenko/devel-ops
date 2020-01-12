2ab787a291ee04ab068ea4e692abf71425f44a87
Alex Jenkinson <alex.jenkinson@verdite.co.uk>
2020-01-12 07:20:50 +0000

    Installed Bulma

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Head -->
+<?php $title= "Devel-Ops"; require_once("includes/head.php"); ?>
+<!-- / Head -->
+
+<body>
+    <!-- Navbar -->
+    <?php require_once("includes/navbar.php"); ?>
+    <!-- / Navbar -->
+
+    <section class="section">
+        <div class="container">
+            <h1 class="title">
+                Hello World
+            </h1>
+            <p class="subtitle">
+                My first website with <strong>Bulma</strong>!
+            </p>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <?php require_once("includes/footer.php"); ?>
+    <!-- / Footer -->
+
+    <!-- Scripts -->
+    <?php require_once("includes/scripts.php"); ?>
+    <!-- / Scripts -->
+</body>
+
+</html>
