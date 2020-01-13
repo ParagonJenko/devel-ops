1d14182772f5887f0921433159158881c75f6fdc
Alex Jenkinson <alex.jenkinson@verdite.co.uk>
2020-01-13 09:22:31 +0000

    Added favicon

--- includes/head.php
+++ includes/head.php
@@ -3,4 +3,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="css/mystyles.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png?v=69B7MAN7Oq">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png?v=69B7MAN7Oq">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png?v=69B7MAN7Oq">
+    <link rel="manifest" href="images/favicon/site.webmanifest?v=69B7MAN7Oq">
+    <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg?v=69B7MAN7Oq" color="#5bbad5">
+    <link rel="shortcut icon" href="images/favicon/favicon.ico?v=69B7MAN7Oq">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="images/favicon/browserconfig.xml?v=69B7MAN7Oq">
+    <meta name="theme-color" content="#ffffff">
+
 </head>
